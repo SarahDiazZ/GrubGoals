a71e32ab5106a41b436ea76dcaef6d460b6da269
Sarah Diaz <diazsarah30@gmail.com>
2024-10-02 20:36:35 -0600

    Added a proxy so that the Vite server can forward API requests to express back-end

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import { createServer } from 'node:http';
+import { join } from 'node:path';
+const PORT = 5173;
+const app = express();
+const server = createServer(app);
+
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/grubgoals");
+}
+
+main().then(function() {
+    console.log("Mongoose connected!");
+}).catch(err => console.log(err));
+
+import userModel from './src/models/user.js';
