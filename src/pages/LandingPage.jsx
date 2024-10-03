ed9d0ed3bbbe0c0be93ba3c637ba47ce3db857ae
Sarah Diaz <diazsarah30@gmail.com>
2024-10-02 22:36:34 -0600

    Added Sign Up and Log In buttons

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,9 +1,13 @@
-import react from 'react'
+import react from 'react';
+import SignUpButton from '../components/SignUpButton';
+import LogInButton from '../components/LogInButton';
 
 function LandingPage(){
     return (
         <div className="background">
             <h1>Grub Goals</h1>
+            <SignUpButton /> {SignUpButton}
+            <LogInButton /> {LogInButton}
         </div>
     );
 }
