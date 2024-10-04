426113cd84b132a89c1987a547fc089f57db4bc4
Austin Wilson <wilsonaustin117@gmail.com>
2024-10-04 13:48:07 -0600

    Initial Route Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import LandingPage from './pages/LandingPage'
+import SignUpPage from './pages/SignupPage'
 import './App.css'
+// import { Router } from 'express';
+// import { Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App(){
   return (
-    <div>
-      <LandingPage/> {/* apprently this should load the landing page - me sleepy */} 
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />  
+          <Route path="/signup" element={<SignUpPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/SignupPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function SignupPage() {
+    return (
+        <div className="background">
+            <div className="container" style={{color:"rgb(197, 87, 53)"}}>
+                <br/>
+                TEST
+            </div>
+        </div>
+    );
+}
+
+export default SignupPage;  // This is the default export
+
