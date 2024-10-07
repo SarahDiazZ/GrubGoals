fd4fdf8a3a4615998c449dcaa3ec57f44f960dda
Mariano Garcia <55813590+MGarcia25@users.noreply.github.com>
2024-10-07 09:22:05 -0600

    Added WalmartAPI menu

---
+++ WalmartAPI/src/main/java/com/GrubGoals/APIKeys.java
@@ -0,0 +1,40 @@
+package com.GrubGoals;
+
+import java.util.ArrayList;
+
+public class APIKeys {
+    public ArrayList<String> getKeys() {
+        String consumerId = "b6eeab12-1bfa-4587-9f4b-a96f1d6940b5";
+        String key = "MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCpE6XjveXy4Ndq\n" + //
+            "AayPAvmCoZoErSE/tcEF1xO642ZS+PGmmI64xYGZuY7moomfg7ih46BYroulMjrh\n" + //
+            "IF3YUCNbMUG9Q81m9URI5hlS2oGwiCkS33A5usKHhZVzT7b4e8njfgvpgw59G0Rg\n" + //
+            "j9BmCEPXDBHIjM/EDwOQvb6tNprzjZ3X9QRnL7licnOErjT3MQtOmTszqI6UkH9A\n" + //
+            "wKIBn64Cw2llpnYlgEh293rPYZWCVjfNbfun4i51DbT453RyCWKem+UXiAyNcaRd\n" + //
+            "HPQUfkymolytLCYuVSLB+M9K6S648F+uzRkuhy1onUKBW0gGHygxzRs8SKKqJmpY\n" + //
+            "upDRSlwxAgMBAAECggEBAItn29vPc42yvWp2PuUbusZuoCNC7KsvHRvHnkj1viSj\n" + //
+            "hfuqxeKrVpR9XsnjMj4HyXAR0t/L6vAmXk3I9heo274Q+vcmtHEzwT2qGy8pYs0/\n" + //
+            "Nz6MZGf1CPfcuMD+9QokZu3fu8qpeITGurYzXc0C+4yQMwl8sMIi7vmkuVbwMwqk\n" + //
+            "Xdh/Of7sU34slMoObB/nCp0q97mJTtOgAgx/9gqusqvBjnAKwUSKxiQb3CI0MgUW\n" + //
+            "4MacfT20q33r53m7tmdxnM+khI7jh1r+zLO+NnM9BoS2+Vpu9OgmiNB55dqk6c3w\n" + //
+            "827CaaTTjOl0yItS9BDzblBbO60V1iSp3ZzMq6mPGRECgYEA3KdbPH7ZOYq0RJ7e\n" + //
+            "Cl9KoGCMPwCVgZIqog6Usfva9Gz1n9ApOpECOKNuC0k1lpZNxgw0hi5+EdCqCN8b\n" + //
+            "boBiD2q0FVe39g539u1bdXabSuHJZ8KseFEGZ0mRtibIrf9W8dEoIVmQ6zKtOP8r\n" + //
+            "BiFXICyyqfdpQucMtsgCAgpAZYMCgYEAxCk0esUA46lGvoJ/ipWv1C3UfXA45JV6\n" + //
+            "GFZqAb9dTnw4RyQpGyPyO8vfFwkFtw5nlcqPm5eDVhsxGy4KLf1wzb+jkgfmRKDK\n" + //
+            "mzaWfGSGziPWoo0cLJITz6g4c03hYI2CAAMZuBsEAiKGRBCG3QCo4WZxnSM1qJhn\n" + //
+            "vz67If32fTsCgYBGzTicV8OSECy53BQ5VcxXW8OEr8ZJVkJxz6Xs2niud4v1JNEl\n" + //
+            "5sPEssQrwOj+EeqQwvfTmnV/d+ongJeHxJAyjDWOp5/yxz+9Lvpq3Y0E4SBEOBYp\n" + //
+            "YS5h1Bx6S0WXd1hJrahn0xqKLa+hm1cN14LiYxVNLoNLIWJWACWMzAdPywKBgEGI\n" + //
+            "GnYhNkjYFkAyzHT5yMGzhddaFEeZAC79P3DSA1Pi/9IUbH8apSQA2ciSRQyAjW5n\n" + //
+            "O06ZcVDkyKTx3oi6sGKr8PMqHG004jq0I9Xup3JaWqv3REBHlMK8yg8LJ43f+FwU\n" + //
+            "rRTqQY6NBhnbA5L1U3XpiW8bgiOOtqoXROD4PM5dAoGBAKX88BN9/05m7llRf8U4\n" + //
+            "69b2KHFG6NsT7nxe5fkacx3jaAn+v09tDXvU60Dl7gsZttsP6G+PoD4TvKKpMxBg\n" + //
+            "tpmkUip5PDTpF/TaYMatiEUUEfKBVin41Me2bkcd7l36sap+REpo60YPdj7LjKXO\n" + //
+            "nfoYJllwxvM+PFEi6RSu64BJ";
+        ArrayList<String> keys = new ArrayList<String>();
+        keys.add(consumerId);
+        keys.add(key);
+        return keys;
+    }
+
+}
---
+++ WalmartAPI/src/main/java/com/GrubGoals/SignatureGenerator.java
@@ -0,0 +1,97 @@
+package com.GrubGoals;
+
+import java.io.IOException;
+import java.io.ObjectStreamException;
+import java.net.MalformedURLException;
+import java.security.KeyRep;
+import java.security.PrivateKey;
+import java.security.Signature;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import org.apache.commons.codec.binary.Base64;
+
+public class SignatureGenerator {
+
+    public Map<String, String> apiHeaders() {
+        APIKeys keys = new APIKeys();
+        ArrayList<String> keysList = keys.getKeys();
+        SignatureGenerator generator = new SignatureGenerator();
+        String priviateKeyVersion = "1";
+        String key = keysList.get(0);
+        String consumerId = keysList.get(1);
+
+        long intimestamp = System.currentTimeMillis();
+
+        System.out.println("consumerId: " + consumerId);
+        System.out.println("intimestamp: " + intimestamp);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("WM_CONSUMER.ID", consumerId);
+        map.put("WM_CONSUMER.INTIMESTAMP", Long.toString(intimestamp));
+        map.put("WM_SEC.KEY_VERSION", priviateKeyVersion);
+
+        String[] array = canonicalize(map);
+
+        String data = null;
+
+        try {
+            data = generator.generateSignature(key, array[1]);
+        } catch(Exception e) { }
+        
+        Map<String, String> headers = new HashMap<>();
+        headers.put("WM_SEC.KEY_VERSION", priviateKeyVersion);
+        headers.put("WM_CONSUMER.ID", consumerId);
+        headers.put("WM_CONSUMER.INTIMESTAMP", Long.toString(intimestamp));
+        headers.put("WM_SEC.AUTH_SIGNATURE", data);
+
+        return headers;
+    }
+
+    public String generateSignature(String key, String stringToSign) throws Exception {
+        Signature signatureInstance = Signature.getInstance("SHA256WithRSA");
+
+        ServiceKeyRep keyRep = new ServiceKeyRep(KeyRep.Type.PRIVATE, "RSA", "PKCS#8", Base64.decodeBase64(key));
+
+        PrivateKey resolvedPrivateKey = (PrivateKey) keyRep.readResolve();
+
+        signatureInstance.initSign(resolvedPrivateKey);
+
+        byte[] bytesToSign = stringToSign.getBytes("UTF-8");
+        signatureInstance.update(bytesToSign);
+        byte[] signatureBytes = signatureInstance.sign();
+
+        String signatureString = Base64.encodeBase64String(signatureBytes);
+
+        return signatureString;
+    }
+
+    protected static String[] canonicalize(Map<String, String> headersToSign) {
+        StringBuffer canonicalizedStrBuffer=new StringBuffer();
+        StringBuffer parameterNamesBuffer=new StringBuffer();
+        Set<String> keySet=headersToSign.keySet();
+
+        // Create sorted key set to enforce order on the key names
+        SortedSet<String> sortedKeySet=new TreeSet<String>(keySet);
+        for (String key :sortedKeySet) {
+            Object val=headersToSign.get(key);
+            parameterNamesBuffer.append(key.trim()).append(";");
+            canonicalizedStrBuffer.append(val.toString().trim()).append("\n");
+        }
+        return new String[] {parameterNamesBuffer.toString(), canonicalizedStrBuffer.toString()};
+    }
+
+    class ServiceKeyRep extends KeyRep  {
+        private static final long serialVersionUID = -7213340660431987616L;
+        public ServiceKeyRep(Type type, String algorithm, String format, byte[] encoded) {
+            super(type, algorithm, format, encoded);
+        }
+        protected Object readResolve() throws ObjectStreamException {
+            return super.readResolve();
+        }
+    }
+}
