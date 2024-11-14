d2c465f2e9640a863122287715bbfefc1f915ad3
Mariano Garcia <55813590+MGarcia25@users.noreply.github.com>
2024-11-14 15:03:54 -0700

    changed from required to import

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,10 @@
-const flowbite = require("flowbite-react/tailwind");
+import flowbite from "flowbite-react/tailwind";
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}",
-    flowbite.content(),
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    flowbite.plugin(),
-  ],
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
+	theme: {
+		extend: {},
+	},
+	plugins: [flowbite.plugin()],
 };
-
